abae2254a6654795a739dd03b46f357505a47020
AdiotChezBurga <amuah7109@icloud.com>
2025-07-05 13:08:51 +0000

    updated

--- module.js
+++ module.js
@@ -1,12 +1,11 @@
 import { createInterface } from "readline";
 
-
-const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
 export const input = (message) => {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    
     rl.question(message,(answer) => {
       rl.close();
       return answer;
