b4b949d300826b72444aa9148e86bde3f7bacbce
AdiotChezBurga <amuah7109@icloud.com>
2025-07-05 15:04:13 +0000

    updated

--- main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var canvas_1 = require("canvas");
-var screen = (0, canvas_1.createCanvas)(800, 600);
-var draw = screen.getContext('2d');
-draw.fillStyle = 'blue';
-draw.fillRect(400, 300, 50, 50);
--- main.ts
+++
@@ -1,7 +0,0 @@
-import { createCanvas } from "canvas";
-
-let screen = createCanvas(800,600);
-let draw = screen.getContext('2d');
-
-draw.fillStyle = 'blue';
-draw.fillRect(400,300,50,50);
