7ace8b2a8439ad63b2e7591673943c50893c8b68
AdiotChezBurga <amuah7109@icloud.com>
2025-07-05 02:34:10 +0000

    update

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
-import { createInterface } from 'readline';
-
-const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question("What's your name? ", (answer) => {
-  console.log(`Hello, ${answer}!`);
-  rl.close();
-});
+x = 0
