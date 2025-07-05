a279646cb44d36ebedab71d4d37f88912de64024
AdiotChezBurga <amuah7109@icloud.com>
2025-07-05 02:30:46 +0000

    updated

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
-console.log("hello world!");
-let x = 0;
-console.log(`my variable is ${x}.`);
+class point {
+    constructor(x,y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    print_points() {
+        return [this.x, this.y];
+    }
+}
+
+let my_point = point(1,2);
+console.log(`${my_point.print_points()}`);
