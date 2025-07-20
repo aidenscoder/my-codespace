cfd0f6920b38ce5829c5240a50254ec0237f8538
AdiotChezBurga <amuah7109@icloud.com>
2025-07-20 15:56:55 -0400

    Update copy.py

--- copy.py
+++ copy.py
@@ -1,43 +1,53 @@
 import os
 
-def parse_str(*args:str):
+def parse_str(*args: str):
     join = ""
     for arg in args:
         join += arg + "\n"
     return join
 
-with open(RF'node project\.vscode\settings.json','w') as file:
-    command = (
-        RF"cls & cd $dir && tsc $fileName && "
-        RF"node $fileNameWithoutExt.js && " 
-        RF"del $fileNameWithoutExt.js & "
-        RF"python -u configure\\cleanup.py"
+
+with open(R".vscode\settings.json", "w") as file:
+    command: str = (
+        R"cd $dir & cd .. & python -u"
+        R"config/console_script.py &"
+        R"cd $dir && tsc $fileName && "
+        R"node $fileNameWithoutExt.js && "
+        R"del $fileNameWithoutExt.js & cd .. & "
+        R"python -u config/cleanup.py & cd $dir & cd .. ||"
+        R"(echo count.txt > config/count.txt)"
     )
-    
+
     file.write(
         parse_str(
-            '{',
+            "{",
             '    "workbench.editor.empty.hint": "text",',
             '    "code-runner.clearPreviousOutput": true,',
             '    "code-runner.runInTerminal": true,',
             '    "code-runner.executorMap": {',
             '        "javascript": "node",',
-            f'       "typescript": "{command}"'
-                '}',
-            '}'
+            f'       "typescript": "{command}"' "}",
+            "}",
         )
     )
 
-directories: dict[str,list[str]] = {
-    '.':os.listdir(".")
-    #'modules':os.listdir('modules')
-}
+directories: dict[str, list[str]] = {".": os.listdir(".")}
+FILE_EXTENSIONS: tuple = tuple(["js"])
+CANNOTREMOVE: tuple[str] = tuple([None])
 
-CANNOT_REMOVE:set = set()
 
-for name,folder in directories.items():
+for name, folder in directories.items():
     for file in folder:
-        path = RF"{name}\{file}"
-        if os.path.isfile(path) and '.' in path and path.split('.')[1] == 'js':
-            if not path in CANNOT_REMOVE:
-                os.remove(path)
+        if name == ".":
+            file_path = Rf"{file}"
+        else:
+            file = Rf"{name}\{file}"
+
+        if (
+            os.path.isfile(file_path)
+            and "." in file_path
+            and file_path.split(".")[1] == next(i for i in FILE_EXTENSIONS)
+        ):
+            if not file_path in CANNOTREMOVE:
+                os.remove(f"cleaned:{file_path}")
+
